c6ed8df989649556847a3ed40b57aabaaafa05ce
Marcus Freisleben <marcus.freisleben@iis.fraunhofer.de>
2025-02-21 11:01:53 +0100

    fix: reassignment

--- internal/gitlab/env.go
+++ internal/gitlab/env.go
@@ -41,7 +41,7 @@ func InitEnv() (*Env, error) {
 	result.JobID = jobID
 	jobImage, ok := os.LookupEnv("CUSTOM_ENV_CI_JOB_IMAGE")
 	if !ok {
-		jobImage, ok := os.LookupEnv("CUSTOM_ENV_CI_DEFAULT_JOB_IMAGE")
+		jobImage, ok = os.LookupEnv("CUSTOM_ENV_CI_DEFAULT_JOB_IMAGE")
 		if !ok {
 			return nil, fmt.Errorf("%w: CUSTOM_ENV_CI_JOB_IMAGE and CUSTOM_ENV_CI_DEFAULT_JOB_IMAGE are missing", ErrGitLabEnv)
 		}
